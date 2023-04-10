fa6e57e0fbd6ac55709ec26c19f8fdddbba8397c
Дмитрий Кутин <TacoKong@yandex.ru>
2023-04-10 22:57:50 +0300

    fixed dict for template

--- menu/templatetags/menu_tags.py
+++ menu/templatetags/menu_tags.py
@@ -20,18 +20,20 @@ def draw_menu(context, menu_name):
         Q(parent__slug=slug),
         menu__name=menu_name
     ).distinct()
-    print(f'{str(nodes.query)=}')
+    children = {}
     result_node_list = []
     nodes_index = len(nodes) - 1
     while (nodes_index >= 0):
         node = nodes[nodes_index]
-        if (not hasattr(node, 'subnodes')):
+        if (node.id in children):
+            node.subnodes = children[node.id][::-1]
+        else:
             node.subnodes = []
         if (node.parent):
-            parent_index = nodes.index(node.parent)
-            if (not hasattr(nodes[parent_index], 'subnodes')):
-                nodes[parent_index].subnodes = []
-            nodes[parent_index].subnodes.append(node)
+            if (node.parent.id in children):
+                children[node.parent.id].append(node)
+            else:
+                children[node.parent.id] = [node]
         else:
             result_node_list.append(node)
         nodes_index -= 1
